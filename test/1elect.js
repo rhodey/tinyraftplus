a5dee0cc3a1f9f0e788e345e5553afce36386dfc
rhodey <rhodey@anhonesteffort.org>
2025-01-25 16:28:47 -0500

    move some stuff into test/util.js

--- test/1elect.js
+++ test/1elect.js
@@ -1,39 +1,6 @@
 const test = require('tape')
 const lib = require('../index.js')
-
-function open(comms, n=1) {
-  const nodes = []
-  for (let i = 1; i <= n; i++) { nodes.push(i) }
-  return nodes.map((id) => {
-    const send = (to, msg) => comms.send(to, id, msg)
-    const node = lib.open(id, nodes, send)
-    comms.register(node)
-    return node
-  })
-}
-
-function comms() {
-  const nodes = []
-  const register = (node) => nodes.push(node)
-
-  function send(to, from, msg) {
-    const node = nodes.find((node) => node.nodeId === to)
-    if (!node) { throw new Error(`node ${from} send to ${to} not found`) }
-    node.onReceive(from, msg)
-  }
-
-  return { register, send }
-}
-
-const sleep = (ms) => new Promise((res, rej) => setTimeout(res, ms))
-
-const start = (nodes) => nodes.forEach((node) => node.start())
-
-const stop = (nodes) => nodes.forEach((node) => node.stop())
-
-const leaders = (nodes) => nodes.filter((node) => node.state === 'leader')
-
-const followers = (nodes) => nodes.filter((node) => node.state === 'follower')
+const { open, comms, sleep, start, stop, leaders, followers } = require('./util.js')
 
 test('test elect n=3', async (t) => {
   t.plan(2)
