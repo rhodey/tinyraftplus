3734ad7280b3ebbe2f58c3e0342f9185b1398358
rhodey <rhodey@anhonesteffort.org>
2025-01-26 17:09:43 -0500

    save

--- test/3log.js
+++ test/3log.js
@@ -1,11 +1,11 @@
 const test = require('tape')
 const { TinyRaftLog } = require('../index.js')
 
-test('test open and append 3', async (t) => {
+test('test start and append 3', async (t) => {
   t.plan(11)
   const log = new TinyRaftLog()
 
-  await log.open()
+  await log.start()
   t.equal(log.seq, -1, 'seq = -1')
   let head = await log.head()
   t.equal(head, null, 'head = null')
@@ -31,14 +31,14 @@ test('test open and append 3', async (t) => {
   head = await log.head()
   t.deepEqual(data, head, 'head = data')
 
-  t.teardown(() => log.close())
+  t.teardown(() => log.stop())
 })
 
 test('test append out of order', async (t) => {
   t.plan(2)
   const log = new TinyRaftLog()
 
-  await log.open()
+  await log.start()
   await log.append(0, {})
   await log.append(1, {})
 
@@ -48,14 +48,14 @@ test('test append out of order', async (t) => {
   seq = await log.append(4, {})
   t.equal(seq, 2, 'seq = 2')
 
-  t.teardown(() => log.close())
+  t.teardown(() => log.stop())
 })
 
 test('test append and remove', async (t) => {
   t.plan(3)
   const log = new TinyRaftLog()
 
-  await log.open()
+  await log.start()
   await log.append(0, {})
   const data = { a: 1}
   await log.append(1, data)
@@ -69,5 +69,5 @@ test('test append and remove', async (t) => {
   const head = await log.head()
   t.deepEqual(data, head, 'head = data')
 
-  t.teardown(() => log.close())
+  t.teardown(() => log.stop())
 })
