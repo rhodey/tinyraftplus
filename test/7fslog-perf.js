e4f6a90df53f967a38a319582afdef6043ac2e49
rhodey <rhodey@anhonesteffort.org>
2025-02-02 19:26:54 -0500

    add test for append larger buf. about 300 append per second.

--- test/7fslog-perf.js
+++ test/7fslog-perf.js
@@ -7,7 +7,7 @@ const toBuf = (obj) => {
   return Buffer.from(obj, 'utf8')
 }
 
-test('test append many', async (t) => {
+test('test append 100 small', async (t) => {
   t.plan(1)
   const log = new FsLog('/tmp/', 'test')
   await log.del()
@@ -16,7 +16,7 @@ test('test append many', async (t) => {
   const data = []
   const count = 100
   for (let i = 0; i < count; i++) {
-    data.push(toBuf({ a: i }))
+    data.push(toBuf({ i }))
   }
 
   const begin = Date.now()
@@ -29,10 +29,42 @@ test('test append many', async (t) => {
 
   const seconds = ms / 1000
   let avg = (count / seconds).toFixed(1)
-  console.log(`${avg}/s`)
+  console.log(`${avg} append per second`)
 
   avg = (avg / 1000).toFixed(2)
-  console.log(`${avg}/ms`)
+  console.log(`${avg} append per ms`)
+
+  t.pass('ok')
+  t.teardown(() => log.stop())
+})
+
+test('test append 100 large', async (t) => {
+  t.plan(1)
+  const log = new FsLog('/tmp/', 'test')
+  await log.del()
+  await log.start()
+
+  const data = []
+  const count = 100
+  const large = new Array(1024).fill('a').join('')
+  for (let i = 0; i < count; i++) {
+    data.push(toBuf({ i, large }))
+  }
+
+  const begin = Date.now()
+  for (const buf of data) {
+    await log.append(buf)
+  }
+
+  const ms = Date.now() - begin
+  console.log(`done ${count} in ${ms}ms`)
+
+  const seconds = ms / 1000
+  let avg = (count / seconds).toFixed(1)
+  console.log(`${avg} append per second`)
+
+  avg = (avg / 1000).toFixed(2)
+  console.log(`${avg} append per ms`)
 
   t.pass('ok')
   t.teardown(() => log.stop())
