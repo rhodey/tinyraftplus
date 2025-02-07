c8f2ddb8c15b108850286e6e69924e5c14666697
rhodey <rhodey@anhonesteffort.org>
2025-02-07 16:21:14 -0500

    save truncate tests

--- test/8multi.js
+++ test/8multi.js
@@ -84,3 +84,74 @@ async function testAppendStartStopNew(t, encoder) {
 test('test append, stop, start, append, new, append', (t) => testAppendStartStopNew(t, new Encoder()))
 // test('test append, stop, start, append, new, append - xxhash body', (t) => testAppendStartStopNew(t, new XxHashEncoder()))
 // test('test append, stop, start, append, new, append - xxhash no body', (t) => testAppendStartStopNew(t, new XxHashEncoder(false)))
+
+async function testTruncate(t, encoder) {
+  t.plan(21)
+  const opts = { encoder, maxLogLen: 64 }
+  let log = new MultiFsLog('/tmp/', 'test', opts)
+  t.teardown(() => log.stop())
+
+  await log.del()
+  await log.start()
+
+  await log.truncate('-1')
+  t.equal(log.seq, '-1', 'seq = -1')
+  t.equal(log.head, null, 'head = null')
+  t.equal(log.logs.length, 0, 'logs = 0')
+
+  let data = []
+  data.push(Buffer.from(new Array(32).fill('a').join('')))
+  await log.append(data[0])
+  t.equal(log.logs.length, 1, 'logs = 1')
+
+  data.push(Buffer.from(new Array(48).fill('b').join('')))
+  await log.append(data[1])
+  t.equal(log.logs.length, 2, 'logs = 2')
+
+  data.push(Buffer.from(new Array(16).fill('c').join('')))
+  await log.append(data[2])
+  t.equal(log.logs.length, 2, 'logs = 2')
+
+  await log.truncate('-1')
+  t.equal(log.seq, '-1', 'seq = -1')
+  t.equal(log.head, null, 'head = null')
+  t.equal(log.logs.length, 0, 'logs = 0')
+
+  data = []
+  data.push(Buffer.from(new Array(32).fill('d').join('')))
+  await log.append(data[0])
+  t.equal(log.logs.length, 1, 'logs = 1')
+
+  data.push(Buffer.from(new Array(48).fill('e').join('')))
+  await log.append(data[1])
+  t.equal(log.logs.length, 2, 'logs = 2')
+
+  data.push(Buffer.from(new Array(16).fill('f').join('')))
+  await log.append(data[2])
+  t.equal(log.logs.length, 2, 'logs = 2')
+
+  await log.truncate('0')
+  t.equal(log.seq, '0', 'seq = 0')
+  t.ok(data[0].equals(log.head), 'head = data again')
+  t.equal(log.logs.length, 1, 'logs = 1')
+
+  data = []
+  data.push(Buffer.from(new Array(32).fill('a').join('')))
+  await log.append(data[0])
+  t.equal(log.logs.length, 1, 'logs = 1')
+
+  data.push(Buffer.from(new Array(48).fill('b').join('')))
+  await log.append(data[1])
+  t.equal(log.logs.length, 2, 'logs = 2')
+
+  data.push(Buffer.from(new Array(16).fill('c').join('')))
+  await log.append(data[2])
+  t.equal(log.logs.length, 2, 'logs = 2')
+
+  await log.truncate('1')
+  t.equal(log.seq, '1', 'seq = 1')
+  t.ok(data[1].equals(log.head), 'head = data again')
+  t.equal(log.logs.length, 2, 'logs = 2')
+}
+
+test('test truncate', (t) => testTruncate(t, new Encoder()))
