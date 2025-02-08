8d748228855d8de64d4c23144c3e145c5c36b5ed
rhodey <rhodey@anhonesteffort.org>
2025-02-08 11:35:09 -0500

    iter tests passing with seq = bigint

--- test/3log-iter.js
+++ test/3log-iter.js
@@ -27,15 +27,15 @@ async function testAppendThreeThenIter(t, encoder) {
     await log.append(toBuf(obj))
   }
 
-  let count = 0
-  for await (let next of log.iter(count.toString())) {
+  let count = 0n
+  for await (let next of log.iter(count)) {
     next = toObj(next)
     t.deepEqual(next, data[count], 'data correct')
     count++
   }
 
   t.pass('no errors')
-  t.equal(count, 3, 'read three bufs')
+  t.equal(count, 3n, 'read three bufs')
 }
 
 test('test append three then iter', (t) => testAppendThreeThenIter(t, new Encoder()))
@@ -56,15 +56,15 @@ async function testAppendOneThenIter(t, encoder) {
     await log.append(toBuf(obj))
   }
 
-  let count = 0
-  for await (let next of log.iter(count.toString())) {
+  let count = 0n
+  for await (let next of log.iter(count)) {
     next = toObj(next)
     t.deepEqual(next, data[count], 'data correct')
     count++
   }
 
   t.pass('no errors')
-  t.equal(count, 1, 'read 1 buf')
+  t.equal(count, 1n, 'read 1 buf')
 }
 
 test('test append one then iter', (t) => testAppendOneThenIter(t, new Encoder()))
@@ -85,16 +85,16 @@ async function testStepSize1(t, encoder) {
     await log.append(toBuf(obj))
   }
 
-  let count = 0
+  let count = 0n
   const optss = { iterStepSize: 1 }
-  for await (let next of log.iter(count.toString(), optss)) {
+  for await (let next of log.iter(count, optss)) {
     next = toObj(next)
     t.deepEqual(next, data[count], 'data correct')
     count++
   }
 
   t.pass('no errors')
-  t.equal(count, 3, 'read three bufs')
+  t.equal(count, 3n, 'read three bufs')
 }
 
 test('test append three then iter step size 1', (t) => testStepSize1(t, new Encoder()))
@@ -115,16 +115,16 @@ async function testStepSize2(t, encoder) {
     await log.append(toBuf(obj))
   }
 
-  let count = 0
+  let count = 0n
   const optss = { iterStepSize: 2 }
-  for await (let next of log.iter(count.toString(), optss)) {
+  for await (let next of log.iter(count, optss)) {
     next = toObj(next)
     t.deepEqual(next, data[count], 'data correct')
     count++
   }
 
   t.pass('no errors')
-  t.equal(count, 3, 'read three bufs')
+  t.equal(count, 3n, 'read three bufs')
 }
 
 test('test append three then iter step size 2', (t) => testStepSize2(t, new Encoder()))
@@ -145,16 +145,16 @@ async function testStepSize3(t, encoder) {
     await log.append(toBuf(obj))
   }
 
-  let count = 0
+  let count = 0n
   const optss = { iterStepSize: 3 }
-  for await (let next of log.iter(count.toString(), optss)) {
+  for await (let next of log.iter(count, optss)) {
     next = toObj(next)
     t.deepEqual(next, data[count], 'data correct')
     count++
   }
 
   t.pass('no errors')
-  t.equal(count, 3, 'read three bufs')
+  t.equal(count, 3n, 'read three bufs')
 }
 
 test('test append three then iter step size 3', (t) => testStepSize3(t, new Encoder()))
@@ -175,16 +175,16 @@ async function testStepSize4(t, encoder) {
     await log.append(toBuf(obj))
   }
 
-  let count = 0
+  let count = 0n
   const optss = { iterStepSize: 4 }
-  for await (let next of log.iter(count.toString(), optss)) {
+  for await (let next of log.iter(count, optss)) {
     next = toObj(next)
     t.deepEqual(next, data[count], 'data correct')
     count++
   }
 
   t.pass('no errors')
-  t.equal(count, 3, 'read three bufs')
+  t.equal(count, 3n, 'read three bufs')
 }
 
 test('test append three then iter step size 4', (t) => testStepSize4(t, new Encoder()))
@@ -205,21 +205,21 @@ async function testIterWithStop(t, encoder) {
     await log.append(toBuf(obj))
   }
 
-  let count = 0
+  let count = 0n
   const optss = { iterStepSize: 1 }
   try {
-    for await (let next of log.iter(count.toString(), optss)) {
+    for await (let next of log.iter(count, optss)) {
       next = toObj(next)
       t.deepEqual(next, data[count], 'data correct')
       count++
-      if (count === 1) { await log.stop() }
+      if (count === 1n) { await log.stop() }
     }
   } catch (err) {
     t.ok(err.message.includes('iter is not open'), 'error thrown')
   }
 
   t.pass('no errors')
-  t.equal(count, 1, 'read 1 buf')
+  t.equal(count, 1n, 'read 1 buf')
 }
 
 test('test append three then iter with stop and step size 1', (t) => testIterWithStop(t, new Encoder()))
@@ -240,8 +240,8 @@ async function testIterEnd(t, encoder) {
     await log.append(toBuf(obj))
   }
 
-  let count = 0
-  const iter = log.iter(count.toString())
+  let count = 0n
+  const iter = log.iter(count)
   await log.append(toBuf({ d: 4 }))
   for await (let next of iter) {
     next = toObj(next)
@@ -250,7 +250,7 @@ async function testIterEnd(t, encoder) {
   }
 
   t.pass('no errors')
-  t.equal(count, 3, 'read three bufs')
+  t.equal(count, 3n, 'read three bufs')
 }
 
 test('test iter ends based off seq at time of create', (t) => testIterEnd(t, new Encoder()))
@@ -271,11 +271,11 @@ async function testIterEnd2(t, encoder) {
     await log.append(toBuf(obj))
   }
 
-  let seq = 3
-  const iter = log.iter(seq.toString())
+  let seq = 3n
+  const iter = log.iter(seq)
   for await (let next of iter) { seq++ }
 
-  t.equal(seq, 3, 'read no bufs')
+  t.equal(seq, 3n, 'read no bufs')
 }
 
 test('test iter returns nothing if seq > log.seq', (t) => testIterEnd2(t, new Encoder()))
@@ -296,9 +296,9 @@ async function testIterWithStop2(t, encoder) {
     await log.append(toBuf(obj))
   }
 
-  let seq = 0
-  const iter = log.iter(seq.toString())
-  await log.truncate('-1')
+  let seq = 0n
+  const iter = log.iter(seq)
+  await log.truncate(-1n)
 
   try {
     for await (let next of iter) { seq++ }
@@ -307,7 +307,7 @@ async function testIterWithStop2(t, encoder) {
     t.ok(err.message.includes('iter is not open'), 'iter stopped')
   }
 
-  t.equal(seq, 0, 'read no bufs')
+  t.equal(seq, 0n, 'read no bufs')
   t.equal(log.iterators.length, 0, 'iter removed')
 }
 
@@ -329,9 +329,9 @@ async function testIterWithStop3(t, encoder) {
     await log.append(toBuf(obj))
   }
 
-  let seq = 0
-  const iter = log.iter(seq.toString())
-  await log.truncate('0')
+  let seq = 0n
+  const iter = log.iter(seq)
+  await log.truncate(0n)
 
   try {
     for await (let next of iter) { seq++ }
@@ -340,7 +340,7 @@ async function testIterWithStop3(t, encoder) {
     t.ok(err.message.includes('iter is not open'), 'iter stopped')
   }
 
-  t.equal(seq, 0, 'read no bufs')
+  t.equal(seq, 0n, 'read no bufs')
   t.equal(log.iterators.length, 0, 'iter removed')
 }
 
@@ -362,17 +362,17 @@ async function testIterNotStopped(t, encoder) {
     await log.append(toBuf(obj))
   }
 
-  let count = 0
-  const iter = log.iter(count.toString())
+  let count = 0n
+  const iter = log.iter(count)
 
   for (const obj of data) {
     await log.append(toBuf(obj))
   }
 
-  await log.truncate('3')
+  await log.truncate(3n)
   for await (let next of iter) { count++ }
 
-  t.equal(count, 3, 'read 3 bufs')
+  t.equal(count, 3n, 'read 3 bufs')
   t.equal(log.iterators.length, 1, 'iter not removed')
 }
 
@@ -394,17 +394,17 @@ async function testIterNotStopped2(t, encoder) {
     await log.append(toBuf(obj))
   }
 
-  let count = 0
-  const iter = log.iter(count.toString())
+  let count = 0n
+  const iter = log.iter(count)
 
   for (const obj of data) {
     await log.append(toBuf(obj))
   }
 
-  await log.truncate('2')
+  await log.truncate(2n)
   for await (let next of iter) { count++ }
 
-  t.equal(count, 3, 'read 3 bufs')
+  t.equal(count, 3n, 'read 3 bufs')
   t.equal(log.iterators.length, 1, 'iter not removed')
 }
 
@@ -428,15 +428,15 @@ async function testBatchWithIter(t, encoder) {
 
   await log.appendBatch(data.slice(3).map(toBuf))
 
-  let count = 0
-  for await (let next of log.iter(count.toString())) {
+  let count = 0n
+  for await (let next of log.iter(count)) {
     next = toObj(next)
     t.deepEqual(next, data[count], 'data correct')
     count++
   }
 
   t.pass('no errors')
-  t.equal(count, 5, 'read 5 bufs')
+  t.equal(count, 5n, 'read 5 bufs')
 }
 
 test('test append then append batch then iter', (t) => testBatchWithIter(t, new Encoder()))
